d49d80e8204b3cbb0dc60bb49e7d5c63a138a429
Felipefachagas <felipefachagas@gmail.com>
2026-08-04 20:35:57 -0300

    corrigindo noticias e deixando responsivo

--- scripts/dom.js
+++ scripts/dom.js
@@ -7,6 +7,8 @@ const noticias = document.querySelector("#noticias");
 const resumos = document.querySelector("#resumos");
 const botaoVerMais = document.querySelector("#ver-mais");
 const botaoVoltar = document.querySelector("#voltar");
+const secaoNoticias = document.querySelector("#secao-noticias");
+const secaoBase = document.querySelector("#secao-base");
 
 function injetarTopo(nomeLiga, icone, fase, timeCasa, gols, timeFora) {
   return `
@@ -24,54 +26,43 @@ function injetarTopo(nomeLiga, icone, fase, timeCasa, gols, timeFora) {
   `;
 }
 
+
 // 3. A Lógica de Roteamento
 function carregarTela(tela) {
   body.className = "";
 
   if (tela === "inicio") {
+    // ESCONDE AS SEÇÕES DE NOTÍCIAS E RESUMOS NO INÍCIO
+    secaoNoticias.style.display = "none";
+    secaoBase.style.display = "none";
+
     destaque.innerHTML = `
       <div style="text-align: center;">
           <h1>Dashboard UEFA ⚽</h1>
           <p style="color: #aaa; margin-top: 10px;">Selecione uma competição no menu para alterar o tema.</p>
       </div>
     `;
-    noticias.innerHTML = ""; // Limpa as notícias na home
-  } else if (tela === "champions") {
-    body.classList.add("tema-champions");
 
-    destaque.innerHTML = injetarTopo(
-      "Champions League",
-      "🏆",
-      "Fase de Grupos",
-      "Real Madrid",
-      "2 - 1",
-      "Dortmund",
-    );
-    noticias.innerHTML = `<p style="color: #000;">Carregando notícias da Champions...</p>`;
-  } else if (tela === "europa") {
-    body.classList.add("tema-europa");
+  } else {
+    // MOSTRA AS SEÇÕES ASSIM QUE QUALQUER LIGA FOR CLICADA
+    secaoNoticias.style.display = "block";
+    secaoBase.style.display = "block";
 
-    destaque.innerHTML = injetarTopo(
-      "Europa League",
-      "🟠",
-      "Semifinal - Ida",
-      "Atalanta",
-      "1 - 1",
-      "Bayer Leverkusen",
-    );
-    noticias.innerHTML = `<p style="color: #000;">Carregando notícias da Europa...</p>`;
-  } else if (tela === "conference") {
-    body.classList.add("tema-conference");
-
-    destaque.innerHTML = injetarTopo(
-      "Conference League",
-      "🟢",
-      "Semifinal - Ida",
-      "Fiorentina",
-      "0 - 0",
-      "West Ham",
-    );
-    noticias.innerHTML = `<p style="color: #000;">Carregando notícias da Conference...</p>`;
+    if (tela === "champions") {
+      body.classList.add("tema-champions");
+      destaque.innerHTML = injetarTopo("Champions League", "🏆", "Fase de Grupos", "Real Madrid", "2 - 1", "Dortmund");
+      filtrarNoticiasPorLiga("champions");
+    }
+    else if (tela === "europa") {
+      body.classList.add("tema-europa");
+      destaque.innerHTML = injetarTopo("Europa League", "🟠", "Semifinal - Ida", "Atalanta", "1 - 1", "Bayer Leverkusen");
+      filtrarNoticiasPorLiga("europa");
+    }
+    else if (tela === "conference") {
+      body.classList.add("tema-conference");
+      destaque.innerHTML = injetarTopo("Conference League", "🟢", "Semifinal - Ida", "Fiorentina", "0 - 0", "West Ham");
+      filtrarNoticiasPorLiga("conference");
+    }
   }
 }
 
@@ -87,31 +78,52 @@ botoesNav.forEach((botao) => {
 
 // Inserindo as noticias
 let indiceAtual = 0;
-let maximoNoticias = 4;
+let maximoNoticias = window.innerWidth <= 768 ? 1 : 4; // Caso seja celular, mostra 1 noticia por vez
 let todasNoticias = [];
+let noticiasFiltradas = [];
+let ultimoCard = 0;
+
+function filtrarNoticiasPorLiga(liga) {
+  if (todasNoticias.length === 0) return;
+  // Cria um novo array só com as noticias da liga esperada do tema
+  noticiasFiltradas = todasNoticias.filter(noticias => noticias.liga == liga);
+  indiceAtual = 0;
+  ultimoCard = 0;
+  atualizarNoticias();
+}
 
 // primeiro carregando os dados do JSON com async func
 async function carregarJSON() {
   try {
     const resposta = await fetch("scripts/noticias.json");
     todasNoticias = await resposta.json();
-    atualizarNoticias();
+    carregarTela("inicio");
   } catch (erro) {
     console.error("Erro ao carregar o JSON:", erro);
   }
 }
 
 // Criando os cards de noticia na tela
-function atualizarNoticias() {
-  const noticiasAparentes = todasNoticias.slice(
-    indiceAtual,
+function atualizarNoticias(direcao = "") {
+  const noticiasAparentes = noticiasFiltradas.slice(
+    ultimoCard,
     indiceAtual + maximoNoticias,
   );
+
+
+  let classeAnimacao = "";
+  if (direcao === "proximo") {
+    classeAnimacao = "anima-proximo";
+  } else if (direcao === "anterior") {
+    classeAnimacao = "anima-anterior";
+  } else {
+    classeAnimacao = "anima-entrada";
+  }
 
   noticias.innerHTML = noticiasAparentes
     .map(
       (noticia) => `
-    <article class="card-noticia">
+    <article class="card-noticia ${classeAnimacao}">
       <img src="${noticia.imagem}" alt="${noticia.titulo}">
       <h3>${noticia.titulo}</h3>
       <p>${noticia.resumo}</p>
@@ -126,7 +138,7 @@ function atualizarNoticias() {
     botaoVoltar.style.display = "block"; // Aparece se ja clicou 1 vez no avançar
   }
 
-  if (indiceAtual + maximoNoticias >= todasNoticias.length) {
+  if (indiceAtual + maximoNoticias >= noticiasFiltradas.length) {
     botaoVerMais.style.display = "none";
   } else {
     botaoVerMais.style.display = "block";
@@ -134,16 +146,18 @@ function atualizarNoticias() {
 }
 
 botaoVerMais.addEventListener("click", () => {
-  if (indiceAtual + maximoNoticias < todasNoticias.length) {
+  if (indiceAtual + maximoNoticias < noticiasFiltradas.length) {
+    ultimoCard = indiceAtual + maximoNoticias;
     indiceAtual += 2;
-    atualizarNoticias();
+    atualizarNoticias("proximo");
   }
 });
 
 botaoVoltar.addEventListener("click", () => {
   if (indiceAtual > 0) {
     indiceAtual -= 2;
-    atualizarNoticias();
+    ultimoCard -= indiceAtual + noticiasFiltradas;
+    atualizarNoticias("anterior");
   }
 });
 
