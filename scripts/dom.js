e548a0fa7978c0a00148c3d8e1d95d838dddcf59
Felipefachagas <felipefachagas@gmail.com>
2026-08-07 09:58:39 -0300

    adicionando noticia e jogadores

--- scripts/dom.js
+++ scripts/dom.js
@@ -1,15 +1,19 @@
 // pegando os dados do html
-const botoesNav = document.querySelectorAll(".nav-btn");
+const botoesNav = document.querySelectorAll(".nav-btn"); // Botões da NavBar
 const app = document.querySelector("#container");
 const body = document.body;
-const destaque = document.querySelector("#destaque");
+const destaque = document.querySelector("#destaque");  // As seções, importante pegar a "caixa" para inserir os dados via DOM
 const noticias = document.querySelector("#noticias");
 const resumos = document.querySelector("#resumos");
-const botaoVerMais = document.querySelector("#ver-mais");
-const botaoVoltar = document.querySelector("#voltar");
+const botaoVerMais1 = document.querySelector("#ver-mais1");  // Para a parte das noticias
+const botaoVoltar1 = document.querySelector("#voltar1");
 const secaoNoticias = document.querySelector("#secao-noticias");
 const secaoBase = document.querySelector("#secao-base");
+const botaoVerMais2 = document.querySelector("#ver-mais2");  // Para os melhores jogadores
+const botaoVoltar2 = document.querySelector("#voltar2");
 
+
+// função enqt nao tem a API
 function injetarTopo(nomeLiga, icone, fase, timeCasa, gols, timeFora) {
   return `
     <h1 style="margin-bottom: 20px;">${icone} ${nomeLiga}</h1>
@@ -27,7 +31,8 @@ function injetarTopo(nomeLiga, icone, fase, timeCasa, gols, timeFora) {
 }
 
 
-// 3. A Lógica de Roteamento
+
+// Carregando a tela completa
 function carregarTela(tela) {
   body.className = "";
 
@@ -52,16 +57,19 @@ function carregarTela(tela) {
       body.classList.add("tema-champions");
       destaque.innerHTML = injetarTopo("Champions League", "🏆", "Fase de Grupos", "Real Madrid", "2 - 1", "Dortmund");
       filtrarNoticiasPorLiga("champions");
+      filtrarJogadoresPorLiga("champions")
     }
     else if (tela === "europa") {
       body.classList.add("tema-europa");
       destaque.innerHTML = injetarTopo("Europa League", "🟠", "Semifinal - Ida", "Atalanta", "1 - 1", "Bayer Leverkusen");
       filtrarNoticiasPorLiga("europa");
+      filtrarJogadoresPorLiga("europa")
     }
     else if (tela === "conference") {
       body.classList.add("tema-conference");
       destaque.innerHTML = injetarTopo("Conference League", "🟢", "Semifinal - Ida", "Fiorentina", "0 - 0", "West Ham");
       filtrarNoticiasPorLiga("conference");
+      filtrarJogadoresPorLiga("conference")
     }
   }
 }
@@ -75,6 +83,11 @@ botoesNav.forEach((botao) => {
     carregarTela(valor);
   });
 });
+
+
+
+
+
 
 // Inserindo as noticias
 let indiceAtual = 0;
@@ -93,7 +106,7 @@ function filtrarNoticiasPorLiga(liga) {
 }
 
 // primeiro carregando os dados do JSON com async func
-async function carregarJSON() {
+async function carregarJSONNoticias() {
   try {
     const resposta = await fetch("scripts/noticias.json");
     todasNoticias = await resposta.json();
@@ -133,19 +146,19 @@ function atualizarNoticias(direcao = "") {
     .join("");
 
   if (indiceAtual <= 0) {
-    botaoVoltar.style.display = "none"; // Esconde se estiver no começo
+    botaoVoltar1.style.display = "none"; // Esconde se estiver no começo
   } else {
-    botaoVoltar.style.display = "block"; // Aparece se ja clicou 1 vez no avançar
+    botaoVoltar1.style.display = "block"; // Aparece se ja clicou 1 vez no avançar
   }
 
   if (indiceAtual + maximoNoticias >= noticiasFiltradas.length) {
-    botaoVerMais.style.display = "none";
+    botaoVerMais1.style.display = "none";
   } else {
-    botaoVerMais.style.display = "block";
+    botaoVerMais1.style.display = "block";
   }
 }
 
-botaoVerMais.addEventListener("click", () => {
+botaoVerMais1.addEventListener("click", () => {
   if (indiceAtual + maximoNoticias < noticiasFiltradas.length) {
     ultimoCard = indiceAtual + maximoNoticias;
     indiceAtual += 2;
@@ -153,12 +166,100 @@ botaoVerMais.addEventListener("click", () => {
   }
 });
 
-botaoVoltar.addEventListener("click", () => {
+botaoVoltar1.addEventListener("click", () => {
   if (indiceAtual > 0) {
     indiceAtual -= 2;
     ultimoCard -= indiceAtual + noticiasFiltradas;
     atualizarNoticias("anterior");
   }
 });
+carregarJSONNoticias();
 
-carregarJSON();
+
+// Seção dos melhores jogadores (lógica bem parecida com a aba das notícias, só muda pq agora pode aparecer 5 ao invés de 4 cards)
+let indice = 0;
+let todosJogadores = [];
+let jogadoresFiltrados = [];
+let maximoJogadores = window.innerWidth <= 768 ? 1 : 5; // 5 ou 1
+
+// Carregando os dados do JSON
+
+// primeiro carregando os dados do JSON com async func
+async function carregarJSONJogadores() {
+  try {
+    const jogadores = await fetch("scripts/jogadores.json");
+    todosJogadores = await jogadores.json();
+    carregarTela("inicio");
+  } catch (erro2) {
+    console.error("Erro ao carregar o JSON:", erro2);
+  }
+}
+
+// Tratamento de dados -> na parte de jogadores, vamos ainda ter o "carrosel", porém vamos permitir 5 jogadores por "tela"
+
+function filtrarJogadoresPorLiga(liga) {
+  if (todosJogadores.length === 0) return;
+  // Cria um novo array só com os jogadores da liga esperada do tema
+  jogadoresFiltrados = todosJogadores.filter(jogador => jogador.liga == liga);
+  indice = 0;
+  atualizarJogadores();
+}
+
+// Função que vai atualizar o DOM da página
+function atualizarJogadores(direcao2 = "") {
+  const jogadoresNaTela = jogadoresFiltrados.slice(indice, indice + maximoJogadores);
+
+  let classeAnimacao2 = "";
+  if (direcao2 === "proximo") {
+    classeAnimacao2 = "anima-proximo";
+  } else if (direcao2 === "anterior") {
+    classeAnimacao2 = "anima-anterior";
+  } else {
+    classeAnimacao2 = "anima-entrada";
+  }
+
+  resumos.innerHTML = jogadoresNaTela
+    .map(
+      (jogador) => `
+    <article class="card-jogador ${classeAnimacao2}">
+      <div class="imagem-container">
+        <img src="${jogador.imagem}" alt="${jogador.nome}" class="imagem-jogador">
+      </div>
+      <div class="dados-jogador">
+        <h3>${jogador.nome}</h3>
+        <p>${jogador.jogos} Jogos</p>
+        <p>${jogador.gols} Gols</p>
+        <p>${jogador.assistencias} Assistências</p>
+      </div>
+    </article>
+  `
+    )
+    .join("");
+
+  if (indice <= 0) {
+    botaoVoltar2.style.display = "none"; // Esconde se estiver no começo
+  } else {
+    botaoVoltar2.style.display = "block"; // Aparece se ja clicou 1 vez no avançar
+  }
+
+  if (indice + maximoJogadores >= jogadoresFiltrados.length) {
+    botaoVerMais2.style.display = "none";
+  } else {
+    botaoVerMais2.style.display = "block";
+  }
+}
+
+botaoVerMais2.addEventListener("click", () => {
+  if (indice + maximoJogadores < jogadoresFiltrados.length) {
+    indice += maximoJogadores;
+    atualizarJogadores("proximo");
+  }
+});
+
+botaoVoltar2.addEventListener("click", () => {
+  if (indice > 0) {
+    indice -= maximoJogadores;
+    atualizarJogadores("anterior");
+  }
+});
+carregarJSONJogadores();
